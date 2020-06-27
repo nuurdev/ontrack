aabf119751e14c9f9c2256c75a3466e59718c86f
nuurdev <52236281+nuurdev@users.noreply.github.com>
2020-06-27 11:15:55 +0100

    Integrated bootstrap and @reach/router (#3)

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import { useLocation } from "@reach/router";
+import { parse } from "query-string";
 
 function App() {
+  const location = useLocation();
+  const searchParams = parse(location.search);
+
+  console.log(searchParams);
+
   return (
-    <div>
-      <h1>OnTrack Application</h1>
-    </div>
+    <Container className="my-5">
+      <Row className="my-3">
+        <h2>OnTrack Application</h2>
+      </Row>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./components/app";
 import * as serviceWorker from "./serviceWorker";
 
+import { Router } from "@reach/router";
+
+import "./styles/global.scss";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App path="/" />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
